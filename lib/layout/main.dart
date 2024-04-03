99315581e194f4c470eefca3b595cce620c168bc
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-04-04 04:38:22 +0900

    fix: birthday card layout 수정

--- lib/layout/main.dart
+++ lib/layout/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
 import 'package:project/layout/model/from_person.dart';
 import 'package:project/layout/model/to_person.dart';
 
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
       home: const _BirthdayCardState(
         toPerson: ToPerson(toName: 'deoky', birthday: '2019년 09월 16일', age: 5),
         fromPerson: FromPerson(fromName: 'mommy'),
-
       ),
     );
   }
@@ -53,45 +51,62 @@ class _BirthdayCardState extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             '♡',
             style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
         ),
-        backgroundColor: Colors.yellow[100],
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Happy\nBirthDay\n${toPerson.toName}!',
-                style: const TextStyle(
-                  color: Colors.pinkAccent,
-                  fontSize: 60,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
+        // backgroundColor: Colors.yellow[100],
+        backgroundColor: Colors.transparent,
+
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('lib/layout/assets/pic.jpeg'), // 배경 이미지
+                  fit: BoxFit.cover,
                 ),
-                textAlign: TextAlign.center,
               ),
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10.0, right: 60.0),
-                  child: Text(
-                    'from. mom',
-                    style: TextStyle(
-                      color: Colors.pinkAccent.shade100,
-                      fontSize: 20,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Happy\nBirthDay\n${toPerson.toName}!',
+                    style: const TextStyle(
+                      color: Colors.pinkAccent,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
                       fontStyle: FontStyle.italic,
                     ),
-                    textAlign: TextAlign.end,
+                    textAlign: TextAlign.center,
                   ),
-                ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, right: 60.0),
+                      child: Text(
+                        'from. mom',
+                        style: TextStyle(
+                          color: Colors.yellow.shade100,
+                          fontSize: 25,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Center(
+                child: Lottie.asset(
+              'lib/layout/assets/birthday_pop.json',
+              width: 600,
+            ))
+          ],
         ),
       ),
     );
